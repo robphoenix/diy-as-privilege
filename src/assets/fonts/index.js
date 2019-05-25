98704175c0d64e4958289a5c0148eb03a9842586
Rob Phoenix <rob@robphoenix.com>
2019-05-25 21:03:13 +0100

    Add font

---
+++ src/assets/fonts/index.js
@@ -0,0 +1,3 @@
+import CocoGothic_BoldItalic from "./CocoGothic-BoldItalic.woff"
+
+export { CocoGothic_BoldItalic }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
 import React from "react"
+import { Global, css } from "@emotion/core"
+import { CocoGothic_BoldItalic } from "../assets/fonts"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return (
+    <React.Fragment>
+      <Global
+        styles={css`
+          @font-face {
+            font-family: "CocoGothic";
+            src: url(${CocoGothic_BoldItalic}) format("woff");
+            font-style: italic;
+            font-weight: bold;
+            text-rendering: optimizeLegibility;
+          }
+          * {
+            font-family: CocoGothic;
+          }
+        `}
+      />
+      <div>Hello world!</div>
+    </React.Fragment>
+  )
+}
