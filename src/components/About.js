2a4e0d2579065ecd31a628b113e7e53b92459323
Rob Phoenix <rob@robphoenix.com>
2019-06-06 17:58:24 +0100

    Better formatting for About

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,17 @@
 import React from "react"
-import { Box, Text } from "@rebass/emotion"
+import { Flex, Text } from "@rebass/emotion"
 
 const About = () => {
   return (
-    <Box>
-      <Text>
+    <Flex width={[3 / 4, 1 / 2]}>
+      <Text as="p" fontFamily="body" fontSize={[2, 3, 4]}>
         The manifesto was written by Richard Phoenix, who has spent over ten
         years dedicated to using his ability, privilege and experience to open
-        up the D.I.Y. ethos to musicians with learning disabilities. He has
-        supported the creation of a music scene in Brighton around the long
-        running Rock House night; organised three UK tours for bands with
+        up the D.I.Y. ethos to musicians with learning disabilities.
+        <br />
+        <br />
+        He has supported the creation of a music scene in Brighton around the
+        long running Rock House night; organised three UK tours for bands with
         members with learning disabilities; supported DIY Space For London to be
         the first volunteer run venue in the UK to achieve Bronze accreditation
         for access; and worked as a music facilitator supporting people with
@@ -17,42 +19,55 @@ const About = () => {
         as well as organising countless other gigs, releases, events and
         projects. He has found that these scenes are the most wildly creative,
         egoless, positive, full of the most amazing music, fun and the best he’s
-        ever been a part of. —- This is the second version of the manifesto, it
-        uses the words ‘Disabled people’ rather than ‘People with Disabilities’.
-        This is so the language in the text better reflects the social model of
-        disability. Why I wrote the Manifesto On the 4th December 2006 I saw the
-        bands Beat Express and Heavy Load play in Brighton. It was the first
-        time I had seen musicians with learning disabilities on stage performing
-        and it was one of the best gigs I’ve ever been to. I wondered why I
-        hadn’t heard of or seen these musicians play before. I thought that if I
-        had enjoyed it so much, then other people would too. It also made me
-        realise the opportunities I had as a musician weren’t available to
-        everyone. So, since then I’ve done all I can to support all kinds of
-        people to make all kinds of music, share this music and get others to
-        watch and listen. I think everyone should have the chance to experience
-        a little of what I’ve been lucky enough to experience since 2006. I will
-        forever be grateful for everything I have learnt from being involved
-        with all of this. I wrote the manifesto to try and encourage others to
-        get involved and help make more and more things happen. —- In 2006 I
-        only knew two bands existed, now I can write a list of fifty-three
-        amazing bands and musicians I’ve been lucky enough to see and hear over
-        the years. Some existed for just one night and some have been playing
-        for years, all of them helped change mine and other people’s worlds,
-        even in some small way. I hope this list will only get longer and
-        longer! 2Decks, Beat Express, Berhana, Bubbaloo, Catherine O’Rourke,
-        Cossima, Dancing Rock Queens, Daniel Wakeford, Dedji, Delta 7, Diamond
-        Glaze, DJ Ivon, DJ Skrapz, Do Your Own Thing Band, Electric Fire, Flash
-        Rockers, Fuzzbomb Band, Fuzzbomb Flash Band, Heavy Load, Hit Party
-        Crashers, Jade, Jolene Wild, K-Dog, Lizzie Emeh, Louis Walkden, Luc
-        Eisenbarth, Ludo, Minotor, Pertti Kurikan Nimipäivät, Radical Racoons,
-        Richard Rock n Roll, Richard Watson, Robyn Steward, Rock House Band,
-        Rock House Rap Gang, Rock Penguins, Ryan O’Donovan, Sabien Gator, Sam
-        Castell-Ward, Seany Seany, Sensatronic Lab, Teddy Boys, Teddy Wynettes,
-        The Carbonators, The Coasters, The Daniel Wakeford Experience, The Fish
+        ever been a part of.
+        <br />
+        <br />
+        This is the second version of the manifesto, it uses the words ‘Disabled
+        people’ rather than ‘People with Disabilities’. This is so the language
+        in the text better reflects the social model of disability.
+        <br />
+        <br />
+        Why I wrote the Manifesto
+        <br />
+        <br />
+        On the 4th December 2006 I saw the bands Beat Express and Heavy Load
+        play in Brighton. It was the first time I had seen musicians with
+        learning disabilities on stage performing and it was one of the best
+        gigs I’ve ever been to. I wondered why I hadn’t heard of or seen these
+        musicians play before. I thought that if I had enjoyed it so much, then
+        other people would too. It also made me realise the opportunities I had
+        as a musician weren’t available to everyone. So, since then I’ve done
+        all I can to support all kinds of people to make all kinds of music,
+        share this music and get others to watch and listen. I think everyone
+        should have the chance to experience a little of what I’ve been lucky
+        enough to experience since 2006. I will forever be grateful for
+        everything I have learnt from being involved with all of this. I wrote
+        the manifesto to try and encourage others to get involved and help make
+        more and more things happen.
+        <br />
+        <br />
+        In 2006 I only knew two bands existed, now I can write a list of
+        fifty-three amazing bands and musicians I’ve been lucky enough to see
+        and hear over the years. Some existed for just one night and some have
+        been playing for years, all of them helped change mine and other
+        people’s worlds, even in some small way. I hope this list will only get
+        longer and longer!
+        <br />
+        <br />
+        2Decks, Beat Express, Berhana, Bubbaloo, Catherine O’Rourke, Cossima,
+        Dancing Rock Queens, Daniel Wakeford, Dedji, Delta 7, Diamond Glaze, DJ
+        Ivon, DJ Skrapz, Do Your Own Thing Band, Electric Fire, Flash Rockers,
+        Fuzzbomb Band, Fuzzbomb Flash Band, Heavy Load, Hit Party Crashers,
+        Jade, Jolene Wild, K-Dog, Lizzie Emeh, Louis Walkden, Luc Eisenbarth,
+        Ludo, Minotor, Pertti Kurikan Nimipäivät, Radical Racoons, Richard Rock
+        n Roll, Richard Watson, Robyn Steward, Rock House Band, Rock House Rap
+        Gang, Rock Penguins, Ryan O’Donovan, Sabien Gator, Sam Castell-Ward,
+        Seany Seany, Sensatronic Lab, Teddy Boys, Teddy Wynettes, The
+        Carbonators, The Coasters, The Daniel Wakeford Experience, The Fish
         Police, The Revs, The Stars Band, Too Hot For Candy, Ultimate Thunder,
         We Rise From The Fallen, Zombie Crash
       </Text>
-    </Box>
+    </Flex>
   )
 }
 
