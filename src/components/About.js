af0785f5ec4eb66958c1f7dcab855d4ddf30a594
huxleyboon <richardjamesphoenix@gmail.com>
2019-07-19 14:05:19 +0100

    removing 'Richard' info entirely and moving to 'Why' section
    keep buy & download links from previous change tho!

--- src/components/About.js
+++ src/components/About.js
@@ -60,23 +60,6 @@ const About = () => {
       </Box>
 
       <BodyText>
-        This was written by Richard Phoenix, who has spent over ten years
-        dedicated to using his ability, privilege and experience to open up the
-        D.I.Y. ethos to musicians with learning disabilities.
-        <br />
-        <br />
-        He has supported the creation of a music scene in Brighton around the
-        long running Rock House night; organised three UK tours for bands with
-        members with learning disabilities; supported DIY Space For London to be
-        the first volunteer run venue in the UK to achieve Bronze accreditation
-        for access; and worked as a music facilitator supporting people with
-        learning disabilities to form bands, create music, record and perform,
-        as well as organising countless other gigs, releases, events and
-        projects. He has found that these scenes are the most wildly creative,
-        egoless, positive, full of the most amazing music, fun and the best he’s
-        ever been a part of.
-        <br />
-        <br />
         This is the second version of the manifesto, it uses the words{" "}
         <Italic>‘Disabled people’</Italic> rather than{" "}
         <Italic>‘People with Disabilities’</Italic>. This is so the language in
