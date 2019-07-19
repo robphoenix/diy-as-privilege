a247e8e7d99ea066bd3d2f7fedc6f5ffa060c097
huxleyboon <richardjamesphoenix@gmail.com>
2019-07-19 14:45:35 +0100

    Rejigged text and put in link addresses
    Totally fucked the code, sorry!!

--- src/components/Why.js
+++ src/components/Why.js
@@ -9,28 +9,17 @@ const Why = () => {
     <SectionWrapper>
       <SectionHeading id="why">Why I wrote the Manifesto</SectionHeading>
       <BodyText>
-        On the 4th December 2006 I saw the bands Beat Express and Heavy Load
-        play in Brighton. It was the first time I had seen musicians with
-        learning disabilities on stage performing and it was one of the best
-        gigs I’ve ever been to. I wondered why I hadn’t heard of or seen these
-        musicians play before. I thought that if I had enjoyed it so much, then
-        other people would too. It also made me realise the opportunities I had
-        as a musician weren’t available to everyone. So, since then I’ve done
-        all I can to support all kinds of people to make all kinds of music,
-        share this music and get others to watch and listen. I think everyone
-        should have the chance to experience a little of what I’ve been lucky
-        enough to experience since 2006. I will forever be grateful for
-        everything I have learnt from being involved with all of this. I wrote
-        the manifesto to try and encourage others to get involved and help make
-        more and more things happen.
+        On the 4th December 2006 I saw the bands Beat Express and Heavy Load play in Brighton. It was the first time I had seen musicians with learning disabilities on stage performing and it was one of the best gigs I’ve ever been to. I wondered why I hadn’t heard of or seen these musicians play before. I thought that if I had enjoyed it so much, then other people would too. It also made me realise the opportunities I had as a musician weren’t available to everyone. So, since then I’ve done all I can to support all kinds of people to make all kinds of music, share this music and get others to watch and listen. 
         <br />
         <br />
-        In 2006 I only knew two bands existed, now I can write a list of
-        fifty-three amazing bands and musicians I’ve been lucky enough to see
-        and hear over the years. Some existed for just one night and some have
-        been playing for years, all of them helped change mine and other
-        people’s worlds, even in some small way. I hope this list will only get
-        longer and longer!
+    This includes supporting the creation of a music scene in Brighton around the long running Rock House (link: https://therockhousebrighton.blogspot.com/) night; organising three UK tours for bands with members with learning disabilities through Constant Flux (link: www.constantflux.co.uk) ; supporting DIY Space For London (link: https://diyspaceforlondon.org/) to be the first volunteer run venue in the UK to achieve Bronze accreditation for access; and working for organisations such as Heart n Soul (https://www.heartnsoul.co.uk/), Carousel (http://www.carousel.org.uk/), Club Soda (http://www.clubsoda.org.uk/), Culture Shift (link: https://www.cultureshift.org.uk/) and Stay Up Late (https://stayuplate.org/) as a music facilitator supporting people with learning disabilities to form bands, create music, record and perform; as well as organising countless other gigs, releases, events and projects.
+    <br />
+        <br />
+    I think everyone should have the chance to experience a little of what I’ve been lucky enough to experience since 2006. I will forever be grateful for everything I have learnt from being involved with all of this. I wrote the manifesto to try and encourage others to get involved and help make more and more things happen.
+     <br />
+        <br />
+       In 2006 I only knew two bands existed, now I can write the list below of bands and musicians. Some existed for just one night and some have been playing for years, all of them helped change mine and other people’s worlds, even in some small way. I hope this list will only get longer and longer! 
+- Richard Phoenix (link: richardphoenix.com)
         <br />
         <br />
       </BodyText>
