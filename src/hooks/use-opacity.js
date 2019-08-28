ea370dddc66498d33e8f1f45ed2653baa9f44eac
Rob Phoenix <rob@robphoenix.com>
2019-08-28 16:18:20 +0100

    Fix build

--- src/hooks/use-opacity.js
+++ src/hooks/use-opacity.js
@@ -4,9 +4,10 @@ const useOpacity = limit => {
   limit = limit || 0
   const [opacity, setOpacity] = React.useState(1)
 
-  window.addEventListener("scroll", () => {
-    setOpacity(window.scrollY > limit ? 0 : 1)
-  })
+  typeof window !== `undefined` &&
+    window.addEventListener("scroll", () => {
+      setOpacity(window.scrollY > limit ? 0 : 1)
+    })
 
   return { opacity }
 }
