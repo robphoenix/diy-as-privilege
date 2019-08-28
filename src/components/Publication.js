92d2bb5b9d1845ba0d6bde202d39caa55f6372c0
Rob Phoenix <rob@robphoenix.com>
2019-08-28 10:42:30 +0100

    Tidy up links

--- src/components/Publication.js
+++ src/components/Publication.js
@@ -2,8 +2,8 @@ import React from "react"
 import SectionWrapper from "./SectionWrapper"
 import SectionHeading from "./SectionHeading"
 import BodyText from "./BodyText"
-// @ts-ignore
 import pdf from "../assets/files/2019-DIY-as-Privilege.pdf"
+import greek from "../assets/files/DIY as Privilege - Greek.pdf"
 import ExternalLink from "./ExternalLink"
 import { H4 } from "./Headings"
 import {
@@ -19,13 +19,13 @@ const Publication = () => {
     <SectionWrapper>
       <SectionHeading id="publication">Publication</SectionHeading>
       <BodyText mb={4}>
-        The Manifesto was originally published as a pamphlet by 
-        <ExternalLink href="https://richardjphoenix.bigcartel.com/product/diy-as-privilege">The Fringe and
-        Underground Music Group</ExternalLink> 
-        at Goldsmiths University as part of the ‘Spaces of Making’
-        project. It was launched on Friday 11th May at an event at Lewisham Art
-        House, with readings from myself, Charles Hayward, Marlo Delara & Sharon
-        Gal.
+        The Manifesto was originally published as a pamphlet by{" "}
+        <ExternalLink href="https://richardjphoenix.bigcartel.com/product/diy-as-privilege">
+          The Fringe and Underground Music Group
+        </ExternalLink>{" "}
+        at Goldsmiths University as part of the ‘Spaces of Making’ project. It
+        was launched on Friday 11th May at an event at Lewisham Art House, with
+        readings from myself, Charles Hayward, Marlo Delara & Sharon Gal.
         <br />
         <br />
         Since then it has been published in the August 2018 issue of Maximum
@@ -35,26 +35,31 @@ const Publication = () => {
         in the US.
         <br />
         <br />
-        Available translations:
-    
-        Greek  
-        <ExternalLink href="https://www.dropbox.com/s/vpbmqj4oehfv7y3/DIY%20as%20Privilege%20-%20Greek.pdf?dl=0"download PDF - 
-        Translated by Zina Sarris for the <ExternalLink href="https://www.soundsgoodproject.net/el"Sounds Good Project
+        <H4>Available translations</H4>
+        Greek (<ExternalLink href={greek}>Download the PDF</ExternalLink>)
+        &mdash; Translated by Zina Sarris for the{" "}
+        <ExternalLink href="https://www.soundsgoodproject.net/el">
+          Sounds Good Project
         </ExternalLink>
+        .
         <br />
         <br />
-        Please get in contact (my email is{" "}
-        <Italic>richardjamesphoenix@gmail.com</Italic>) if you're interested in
-        stocking or distributing the manifesto or translation it into another
-        language.
+        Please get in contact if you're interested in stocking or distributing
+        the manifesto or translating it into another language, my email is{" "}
+        <ExternalLink href="mailto: richardjamesphoenix@gmail.com">
+          richardjamesphoenix@gmail.com
+        </ExternalLink>
+        .
       </BodyText>
       <H4>2nd version</H4>
       <BodyText mb={4}>
         200 copies riso-graph printed with teal ink on grey 180gsm matt paper.
-       <br />
-        Re-design in collaboration with 
-        <ExternalLink href="http://seanroyparker.com/">Sean Roy Parker
-        </ExternalLink>.
+        <br />
+        Re-design in collaboration with{" "}
+        <ExternalLink href="http://seanroyparker.com/">
+          Sean Roy Parker
+        </ExternalLink>
+        .
         <br />
         <br />
         <ExternalLink href="https://richardjphoenix.bigcartel.com/product/diy-as-privilege">
@@ -71,9 +76,16 @@ const Publication = () => {
       </BodyText>
       <H4>1st version</H4>
       <BodyText mb={4}>
-        100 copies riso-graph printed with purple ink on grey 120gsm matt paper. <i>Sold Out</i>
+        100 copies riso-graph printed with purple ink on grey 120gsm matt paper.{" "}
+        <em>
+          <b>Sold Out</b>
+        </em>
         <br />
-        200 copies with various print colours and card colours. <i>Sold Out</i>
+        <br />
+        200 copies with various print colours and card colours.{" "}
+        <em>
+          <b>Sold Out</b>
+        </em>
         <br />
         <br />
         <V01Image01 />
