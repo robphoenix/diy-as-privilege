14d6543fda70fe176c3bd1b07fd3d4a2719ac671
Rob Phoenix <rob@robphoenix.com>
2019-09-11 10:08:00 +0100

    Uppercase section headings

--- src/components/SectionHeading.js
+++ src/components/SectionHeading.js
@@ -4,18 +4,19 @@ import { Heading, Flex } from "rebass"
 const SectionHeading = props => {
   const { id, children, ...rest } = props
   return (
-    <Flex justifyContent="center" {...rest}>
+    <Flex justifyContent="center">
       <Heading
         id={id}
         as="h3"
         fontFamily="header"
-        fontSize={[3, 3, 4]}
-        mb={[4, 5]}
-        textAlign="center"
+        fontSize={3}
+        mb={4}
         lineHeight="1.5"
         css={{
           borderBottom: "3px solid",
           display: "inline-block",
+          textTransform: "uppercase",
+          letterSpacing: "0.1em",
         }}
       >
         {children}
