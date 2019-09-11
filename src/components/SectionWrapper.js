c3d4531772808f109ba5a8b2059ff5d005a0e7fd
Rob Phoenix <rob@robphoenix.com>
2019-09-11 10:07:00 +0100

    Remove z-index from section wrapper

--- src/components/SectionWrapper.js
+++ src/components/SectionWrapper.js
@@ -1,22 +1,17 @@
 import React from "react"
-import styled from "styled-components"
 import { Box } from "rebass"
-import { zIndex } from "styled-system"
-
-const OnTop = styled(Box)(zIndex)
 
 const SectionWrapper = props => {
   return (
-    <OnTop
+    <Box
       as="section"
       px={4}
       width={["none", 2 / 3, 1 / 2]}
       mb={[5, 6, 6]}
-      zIndex={5}
       {...props}
     >
       {props.children}
-    </OnTop>
+    </Box>
   )
 }
 
